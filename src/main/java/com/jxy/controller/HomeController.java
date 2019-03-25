c15fb1ac708c460142abe08a3836557ddaf4f448
ybl <1162474984@qq.com>
2019-03-25 16:17:24 +0800

    Rest服务端

--- src/main/java/com/jxy/controller/HomeController.java
+++ src/main/java/com/jxy/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.jxy.controller;
 
+import com.jxy.security.UserDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
@@ -34,4 +39,31 @@ public class HomeController {
         return "login";
     }
 
+    @RequestMapping("getUserInfoResponseEntity")
+    public ResponseEntity<UserDetail> getUserInfoResponseEntity() {
+        UserDetail u = new UserDetail();
+        u.setRole("测试角色");
+        u.setUserName("name");
+        u.setUserPass("pass");
+        return new ResponseEntity<UserDetail>(u, HttpStatus.OK);
+    }
+
+    @RequestMapping("getUserInfo")
+    @ResponseBody
+    public UserDetail getUserInfo(String userName) {
+        UserDetail u = new UserDetail();
+        u.setRole("测试角色");
+        u.setUserName(userName);
+        u.setUserPass("pass");
+        return u;
+    }
+
+    @RequestMapping("getUserInfodx")
+    @ResponseBody
+    public UserDetail getUserInfodx(UserDetail userDetail) {
+        userDetail.setUserName("我改了哈，服务端");
+        userDetail.setRole("role");
+        userDetail.setUserPass("123456");
+        return userDetail;
+    }
 }
---
+++ src/main/java/com/jxy/entity/UserInfo.java
@@ -0,0 +1,9 @@
+package com.jxy.entity;
+
+/**
+ * @Auther: jxy
+ * @Date: 2019/3/25 14:59
+ * @Description:
+ */
+public class UserInfo {
+}
