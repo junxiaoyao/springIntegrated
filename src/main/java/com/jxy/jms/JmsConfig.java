66d1cef3953b5fdda58d1e616644ce18bf91dcb5
ybl <1162474984@qq.com>
2019-03-26 18:20:09 +0800

    Rest服务端

--- src/main/java/com/jxy/jms/JmsConfig.java
+++ src/main/java/com/jxy/jms/JmsConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.jms.ConnectionFactory;
+import javax.jms.IllegalStateException;
 
 /**
  * @Auther: jxy
